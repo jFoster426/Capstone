b86e2bdb0a74655a29ccb22799cc2063a3cf6e14
jFoster426 <juddf@sfu.ca>
2022-07-12 21:25:03 -0700

    display png with upng

---
+++ ESP32_Testing/lvgl_strastech_logo_png/main/main.c
@@ -0,0 +1,269 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_freertos_hooks.h"
+#include "freertos/semphr.h"
+#include "esp_system.h"
+#include "driver/gpio.h"
+
+#include "lvgl.h"
+#include "lvgl_helpers.h"
+
+#include "lv_port_fs.h"
+
+#include "driver/sdspi_host.h"
+
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include "esp_vfs_fat.h"
+#include "sdmmc_cmd.h"
+
+#include "upng.h"
+
+#define TAG "demo"
+#define LV_TICK_PERIOD_MS 1
+
+#define MOUNT_POINT "/sdcard"
+
+#define SPI_MISO        14
+#define SPI_MOSI        11
+#define SPI_CLK         13
+#define SD_CS           10
+#define SD_CD           12
+
+
+static void lv_tick_task(void *arg);
+static void guiTask(void *pvParameter);
+
+upng_t* upng;
+
+void app_main()
+{
+    /* If you want to use a task to create the graphic, you NEED to create a Pinned task
+     * Otherwise there can be problem such as memory corruption and so on.
+     * NOTE: When not using Wi-Fi nor Bluetooth you can pin the guiTask to core 0 */
+    xTaskCreatePinnedToCore(guiTask, "gui", 4096*2, NULL, 0, NULL, 1);
+
+    vTaskDelay(1200 / portTICK_PERIOD_MS);
+
+    // Configure LCD backlight and enable.
+    gpio_config_t n_lcd_bklt_en = { GPIO_NUM_0, GPIO_MODE_OUTPUT, 0, 0, 0 };
+    gpio_config(&n_lcd_bklt_en);
+    gpio_set_direction(GPIO_NUM_0, GPIO_MODE_OUTPUT);
+    gpio_set_level(GPIO_NUM_0, 0);
+
+    // Configure charger enable.
+    gpio_config_t n_chg_en = { GPIO_NUM_17, GPIO_MODE_OUTPUT, 0, 0, 0 };
+    gpio_config(&n_chg_en);
+    gpio_set_direction(GPIO_NUM_17, GPIO_MODE_OUTPUT);
+    gpio_set_level(GPIO_NUM_17, 0);
+    
+    printf("Free RAM: %d\n", esp_get_free_heap_size());
+}
+
+/* Creates a semaphore to handle concurrent call to lvgl stuff
+ * If you wish to call *any* lvgl function from other threads/tasks
+ * you should lock on the very same semaphore! */
+SemaphoreHandle_t xGuiSemaphore;
+
+static void guiTask(void *pvParameter)
+{
+    (void) pvParameter;
+    xGuiSemaphore = xSemaphoreCreateMutex();
+
+    esp_vfs_fat_sdmmc_mount_config_t mount_config =
+    {
+        .format_if_mount_failed = false,
+        .max_files = 5,
+        .allocation_unit_size = 16 * 1024
+    };
+    sdmmc_card_t *card;
+    const char mount_point[] = MOUNT_POINT;
+    ESP_LOGI(TAG, "Initializing SD card");
+    ESP_LOGI(TAG, "Using SPI peripheral");
+
+    sdmmc_host_t host =
+    {
+        .flags = SDMMC_HOST_FLAG_SPI | SDMMC_HOST_FLAG_DEINIT_ARG,
+        .slot = SPI3_HOST,
+        .max_freq_khz = SDMMC_FREQ_DEFAULT,
+        .io_voltage = 3.3f,
+        .init = &sdspi_host_init,
+        .set_bus_width = NULL,
+        .get_bus_width = NULL,
+        .set_bus_ddr_mode = NULL,
+        .set_card_clk = &sdspi_host_set_card_clk,
+        .do_transaction = &sdspi_host_do_transaction,
+        .deinit_p = &sdspi_host_remove_device,
+        .io_int_enable = &sdspi_host_io_int_enable,
+        .io_int_wait = &sdspi_host_io_int_wait,
+        .command_timeout_ms = 0,
+    };
+
+    spi_bus_config_t bus_cfg =
+    {
+        .mosi_io_num = SPI_MOSI,
+        .miso_io_num = SPI_MISO,
+        .sclk_io_num = SPI_CLK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 4000,
+    };
+
+    if (spi_bus_initialize(host.slot, &bus_cfg, SPI_DMA_CH_AUTO) != ESP_OK)
+        ESP_LOGE(TAG, "Failed to initialize bus.");
+
+    // This initializes the slot without card detect (CD) and write protect (WP) signals.
+    // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+    sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
+    slot_config.gpio_cs = SD_CS;
+    slot_config.host_id = host.slot;
+
+    ESP_LOGI(TAG, "Mounting filesystem");
+    esp_err_t ret = esp_vfs_fat_sdspi_mount(mount_point, &host, &slot_config, &mount_config, &card);
+
+    if (ret != ESP_OK)
+    {
+        if (ret == ESP_FAIL)
+        {
+            ESP_LOGE(TAG, "Failed to mount filesystem. "
+                     "If you want the card to be formatted, set the EXAMPLE_FORMAT_IF_MOUNT_FAILED menuconfig option.");
+        } else
+        {
+            ESP_LOGE(TAG, "Failed to initialize the card (%s). "
+                     "Make sure SD card lines have pull-up resistors in place.", esp_err_to_name(ret));
+        }
+        while (1);
+    }
+    ESP_LOGI(TAG, "Filesystem mounted");
+
+    // Card has been initialized, print its properties
+    sdmmc_card_print_info(stdout, card);
+
+    lv_init();
+
+    /* Initialize SPI or I2C bus used by the drivers */
+    lvgl_driver_init();
+
+    // Initialize file system.
+    lv_port_fs_init();
+
+    lv_color_t* buf1 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
+    assert(buf1 != NULL);
+
+    /* Use double buffered when not working with monochrome displays */
+    lv_color_t* buf2 = heap_caps_malloc(DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
+    assert(buf2 != NULL);
+
+    static lv_disp_buf_t disp_buf;
+
+    uint32_t size_in_px = DISP_BUF_SIZE;
+
+    // Initialize the working buffer.
+    lv_disp_buf_init(&disp_buf, buf1, buf2, size_in_px);
+
+    lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.flush_cb = disp_driver_flush;
+
+    disp_drv.buffer = &disp_buf;
+    lv_disp_drv_register(&disp_drv);
+
+    lv_obj_t * img = lv_img_create(lv_scr_act(), NULL);
+
+    upng = upng_new_from_file("/sdcard/helloworld.png");
+    if (upng != NULL)
+    {
+        upng_decode(upng);
+        int err = upng_get_error(upng);
+        printf("UPNG error code: %d\n", err);
+        if (err == UPNG_EOK)
+        {
+            printf("width=%d, height=%d\n", upng_get_width(upng), upng_get_height(upng));
+
+            // We wish to convert the RGBA data from the PNG to RGB565 data.
+            // Allocate some memory to store the new image data.
+            uint16_t *img_rgb565 = (uint16_t *)malloc(320 * 240 * 2);
+
+            printf("upng format : %d\n", upng_get_format(upng));
+
+            uint8_t *oldDataPtr = (uint8_t *)upng_get_buffer(upng);
+
+            uint8_t r, g, gh, gl, b;
+
+            for (uint32_t i = 0; i < 320 * 240; i++)
+            {
+                // Consider changing to RGB332 and test frame-rate.
+                r = *(oldDataPtr) >> 3;
+                g = *(oldDataPtr + 1) >> 2;
+                gh = (g & 0b111000) >> 3;
+                gl = (g & 0b111);
+                b = *(oldDataPtr + 2) >> 3;
+
+                img_rgb565[i] = (((gl << 5) | b) << 8) | ((r << 3) | gh);
+
+                oldDataPtr += 4;
+            }
+            
+            const lv_img_dsc_t img_dsc =
+            {
+                .header.cf = LV_IMG_CF_TRUE_COLOR,
+                .header.always_zero = 0,
+                .header.reserved = 0,
+                .header.w = 320,
+                .header.h = 240,
+                .data_size = 320 * 240 * 2,
+                .data = (uint8_t *)img_rgb565
+            };
+
+
+            lv_img_set_src(img, &img_dsc);
+        }
+    }
+
+    // upng_free(upng);
+
+    //lv_img_set_src(img, &unnamed);
+    //lv_img_set_src(img, "S:/sdcard/wandb.bin");
+
+    printf("image show\n");
+
+    // Create and start a periodic timer interrupt to call lv_tick_inc.
+    const esp_timer_create_args_t periodic_timer_args =
+    {
+        .callback = &lv_tick_task,
+        .name = "periodic_gui"
+    };
+
+    esp_timer_handle_t periodic_timer;
+
+    ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, LV_TICK_PERIOD_MS * 1000));
+
+    while (1)
+    {
+        /* Delay 1 tick (assumes FreeRTOS tick is 10ms */
+        vTaskDelay(pdMS_TO_TICKS(10));
+
+        /* Try to take the semaphore, call lvgl related function on success */
+        if (pdTRUE == xSemaphoreTake(xGuiSemaphore, portMAX_DELAY))
+        {
+            lv_task_handler();
+            xSemaphoreGive(xGuiSemaphore);
+        }
+    }
+    /* A task should NEVER return */
+    free(buf1);
+    free(buf2);
+    vTaskDelete(NULL);
+}
+
+static void lv_tick_task(void *arg)
+{
+    (void) arg;
+    lv_tick_inc(LV_TICK_PERIOD_MS);
+}
